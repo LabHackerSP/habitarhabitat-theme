668a9790147eff0aec8dfd7328c3ab1648b02173
Pedro Markun <pedro@markun.com.br>
2016-11-28 15:25:15 -0200

    adicionando template single page

---
+++ template-parts/content-single.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Template part for displaying single posts.
+ *
+ * @package ultrabootstrap
+ */
+
+?>
+
+<div class="page-title">
+  <h1><?php the_title(); ?></h1>
+</div>
+
+<div class="single-post">
+   <div class="post-content">
+      <?php if (has_post_thumbnail()) : ?>
+        <?php the_post_thumbnail('full'); ?>
+      <?php endif; ?> 
+    
+    <article class="spacer">
+      <?php the_content();?>
+
+      <?php
+        wp_link_pages( array(
+          'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'ultrabootstrap' ),
+          'after'  => '</div>',
+        ) );
+      ?>     
+    </article>
+    </div>
+  </div>
